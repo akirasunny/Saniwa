2b52b2bc7d5dfd2373ca94d76323ccfbda4b1ee4
Kazuki Oishi <oishikazuki@gmail.com>
2016-10-26 21:59:38 +0900

    Add auto resize

--- Window/MainWindow.cpp
+++ Window/MainWindow.cpp
@@ -9,9 +9,11 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),ui(new Ui::MainWin
     this->setWindowTitle("Saniwa");
     connect(ui->webEngineView,SIGNAL(loadFinished(bool)),this,SLOT(onBrowserLoadFinish(bool)));
     ui->webEngineView->setUrl(QUrl("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=825012/"));
+    wRThread = std::unique_ptr<std::thread>(new std::thread(&MainWindow::windowResizeCheck,this));
 }
 MainWindow::~MainWindow() {
-
+    stopAllWindowThread = true;
+    wRThread->detach();
 }
 void MainWindow::onBrowserLoadFinish(bool stat) {
     std::string url = ui->webEngineView->url().toString().toStdString();
@@ -36,9 +38,26 @@ void MainWindow::onBrowserLoadFinish(bool stat) {
 
 void MainWindow::onBrowserWaitComplete() {
     if(ui == nullptr) return;
-    ui->webEngineView->page()->runJavaScript("document.body.style.overflow‌​='hidden';");
+    ui->webEngineView->page()->runJavaScript("document.body.style.overflow = \"hidden\"");
     ui->webEngineView->page()->runJavaScript("$(function(){$('#flash_object').css({'position': 'fixed','left': '0','width': '100%','height': '100%','z-index': '1000000'})});",[this](const QVariant &v){
         cout << v.toString().toStdString() << endl;
     });
     this->resize(960,580);
+}
+
+void MainWindow::onWindowResized() {
+    double resizedHeight = (this->size().width() / 960.0) * 580.0;
+    this->resize(this->size().width(),(int)resizedHeight);
+}
+
+void MainWindow::windowResizeCheck() {
+    QSize lastSize;
+    while (!stopAllWindowThread){
+        lastSize = this->size();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        if(tokenLoadOK && (lastSize.width() != this->size().width() || lastSize.height() != this->size().height())){
+            //call slot on main window thread
+            QMetaObject::invokeMethod(this,"onWindowResized");
+        }
+    }
 }
--- Window/MainWindow.h
+++ Window/MainWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QMainWindow>
 #include <memory>
+#include <thread>
 #include "ui_MainWindow.h"
 
 class MainWindow : public QMainWindow{
@@ -10,8 +11,13 @@ public:
     ~MainWindow();
 private:
     std::unique_ptr<Ui::MainWindow> ui;
+    std::unique_ptr<std::thread> wRThread;
+    void windowResizeCheck();
     bool tokenLoadOK = false;
+    bool resizeLock = false;
+    bool stopAllWindowThread = false;
 private slots:
     void onBrowserLoadFinish(bool stat);
     void onBrowserWaitComplete();
+    void onWindowResized();
 };
