ff6bc11f76c73213a6b77ea43bac5ffc4e8b92ea
Kazuki Oishi <oishikazuki@gmail.com>
2016-10-29 22:46:22 +0900

    Add winmain.cpp

---
+++ winmain.cpp
@@ -0,0 +1,19 @@
+﻿#ifdef _MSC_VER
+
+#include <windows.h>
+#include <iostream>
+#include <memory>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QWebEngineSettings>
+#include "Window/MainWindow.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nShowCmd) {
+    QApplication app(__argc, __argv);
+    QWebEngineSettings::globalSettings()->setAttribute(QWebEngineSettings::PluginsEnabled, true);
+    std::unique_ptr<MainWindow> mainWindow(new MainWindow());
+    mainWindow->show();
+    return app.exec();
+}
+
+#endif // only on MSVC
